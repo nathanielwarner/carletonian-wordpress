55d62540cd971d3cbcc5f2ab2ae33560fe599405
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-02-26 23:47:41 +0000

    Initial commit

---
+++ wp-config.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'bitnami_wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'bn_wordpress');
+
+/** MySQL database password */
+define('DB_PASSWORD', '45bee7e8ac');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost:3306');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', '1b434f900bc9bfc397252d8513e10cd2c1b5d2e49ad52084118c84aff4b360cd');
+define('SECURE_AUTH_KEY', '7f835cc94635c4c4f778a8e227ce36d38413b3e8838f36cc134e3e08c9788616');
+define('LOGGED_IN_KEY', 'e1a0ff088d10c7c6d05fef5c644e2858e76b9297deaea461ded12dae339cb89e');
+define('NONCE_KEY', '5c174494c02164209cfdcf3c30ca5d91fee76097564f14c5eec6fd4f4d066678');
+define('AUTH_SALT', 'c180ae79c99c37ffb7c7547621c4218f206284613afd1f0f3298ebff7962e008');
+define('SECURE_AUTH_SALT', 'f43ada6992b7776ce3e6258a354c41a4fd1732b51451f23a3cd0df32024771fa');
+define('LOGGED_IN_SALT', '6b792f476a303ca05af3ee12cf5abce641763fbf491fba9bbd5df831a9a840f7');
+define('NONCE_SALT', 'e5710b0671625a76a59197b33002354d84b17419eb7e54a8b9c17ba5b009ebdd');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+define('FS_METHOD', 'direct');
+
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+*/
+
+if ( defined( 'WP_CLI' ) ) {
+    $_SERVER['HTTP_HOST'] = 'localhost';
+}
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_TEMP_DIR', '/opt/bitnami/apps/wordpress/tmp');
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://docs.bitnami.com/?page=apps&name=wordpress&section=how-to-re-enable-the-xml-rpc-pingback-feature
+
+if ( !defined( 'WP_CLI' ) ) {
+    // remove x-pingback HTTP header
+    add_filter('wp_headers', function($headers) {
+        unset($headers['X-Pingback']);
+        return $headers;
+    });
+    // disable pingbacks
+    add_filter( 'xmlrpc_methods', function( $methods ) {
+            unset( $methods['pingback.ping'] );
+            return $methods;
+    });
+    add_filter( 'auto_update_translation', '__return_false' );
+}
---
+++ wp-content/plugins/featured-image-from-url/admin/html/js/menu.js
@@ -0,0 +1,66 @@
+jQuery(document).ready(function () {
+    jQuery('.wrap').css('opacity', 1);
+});
+
+function invert(id) {
+    if (jQuery("#fifu_toggle_" + id).attr("class") == "toggleon") {
+        jQuery("#fifu_toggle_" + id).attr("class", "toggleoff");
+        jQuery("#fifu_input_" + id).val('off');
+    } else {
+        jQuery("#fifu_toggle_" + id).attr("class", "toggleon");
+        jQuery("#fifu_input_" + id).val('on');
+    }
+}
+
+jQuery(function () {
+    var url = window.location.href;
+
+    //forms with id started by...
+    jQuery("form[id^=fifu_form]").each(function (i, el) {
+        //onsubmit
+        jQuery(this).submit(function () {
+            save(this);
+        });
+    });
+
+    jQuery("#accordion").accordion();
+    jQuery("#accordionCrop").accordion();
+    jQuery("#accordionClean").accordion();
+    jQuery("#tabs").tabs();
+    jQuery("#tabs-top").tabs();
+    jQuery("#fifu_input_spinner_cron_metadata").spinner({min: 1, step: 1});
+    jQuery("#fifu_input_spinner_db").spinner({min: 100, step: 100});
+    jQuery("#fifu_input_spinner_image").spinner({min: 0});
+    jQuery("#fifu_input_spinner_video").spinner({min: 0});
+    jQuery("#fifu_input_spinner_slider").spinner({min: 0});
+    jQuery("#fifu_input_slider_speed").spinner({min: 0});
+    jQuery("#fifu_input_slider_pause").spinner({min: 0});
+    jQuery("#tabsApi").tabs();
+    jQuery("#tabsPremium").tabs();
+    jQuery("#tabsWooImport").tabs();
+    jQuery("#tabsWpAllImport").tabs();
+});
+
+function save(formName, url) {
+    var frm = jQuery(formName);
+    jQuery.ajax({
+        type: frm.attr('method'),
+        url: url,
+        data: frm.serialize(),
+        success: function (data) {
+            //alert('saved');
+        }
+    });
+}
+
+jQuery(function () {
+    jQuery("#dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        width: "630px",
+    });
+
+    jQuery("#opener").on("click", function () {
+        jQuery("#dialog").dialog("open");
+    });
+});
