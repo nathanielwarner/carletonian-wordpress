e28be163d1594a32e341719225d9ce6ffd2b4a0b
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-03-01 16:26:02 -0600

    Misc styling improvements

--- wp-content/themes/mission-news-carletonian/content/archive-header.php
+++ wp-content/themes/mission-news-carletonian/content/archive-header.php
@@ -8,29 +8,29 @@ if ( ! is_archive() || ( get_theme_mod( 'archive_title' ) == 'no' ) && get_theme
 	return;
 }
 
-$icon_class = 'folder-open';
-$prefix = esc_html_x( 'Posts published in', 'Posts published in CATEGORY', 'mission-news' );
+$icon_class = 'newspaper';
+$prefix = esc_html_x( '', 'Posts published in CATEGORY', 'mission-news' );
 
 if ( is_tag() ) {
 	$icon_class = 'tag';
-	$prefix = esc_html__( 'Posts tagged as', 'mission-news' );
+	$prefix = esc_html__( 'Articles tagged as', 'mission-news' );
 } elseif ( is_author() ) {
 	$icon_class = 'user';
-	$prefix = esc_html_x( 'Posts published by', 'Posts published by AUTHOR', 'mission-news' );
+	$prefix = esc_html_x( 'Articles written by', 'Posts published by AUTHOR', 'mission-news' );
 } elseif ( is_date() ) {
 	$icon_class = 'calendar';
 	// Repeating default value to add new translator note - context may change word choice
-	$prefix = esc_html_x( 'Posts published in', 'Posts published in MONTH', 'mission-news' );
+	$prefix = esc_html_x( 'Articles published in', 'Posts published in MONTH', 'mission-news' );
 }
 ?>
 
 <div class='archive-header'>
 	<?php if ( get_theme_mod( 'archive_title' ) != 'no' ) : ?>
 	<h1>
-		<i class="fas fa-<?php echo esc_attr( $icon_class ); ?>"></i>
+		<i class="far fa-<?php echo esc_attr( $icon_class ); ?>"></i>
 		<?php
 		echo esc_html( $prefix ) . ' ';
-		the_archive_title( '&ldquo;', '&rdquo;' );
+		the_archive_title( '', '' );
 		?>
 	</h1>
 	<?php endif;
