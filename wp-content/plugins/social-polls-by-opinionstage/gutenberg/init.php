28842c2dcc4744c78186cd7f1765d10e1c7232d4
Ubuntu <ubuntu@ip-172-31-82-232.ec2.internal>
2019-03-26 00:23:44 +0000

    Added reCaptcha

--- wp-content/plugins/social-polls-by-opinionstage/gutenberg/init.php
+++ wp-content/plugins/social-polls-by-opinionstage/gutenberg/init.php
@@ -56,24 +56,29 @@ function oswp_gutenberg_enqueue_scripts() {
 		$OswpPluginVersion = OPINIONSTAGE_WIDGET_VERSION;
 
 		// Fetch Url For Ajax Call Opinion Stage
-		$FetchUrlOS = 'https://www.opinionstage.com/api/wp/v1/my/widgets';
+		$FetchUrlOS = OPINIONSTAGE_SERVER_BASE.'/api/wp/v1/my/widgets';
 
 		// Url For Creating New Content OR Template On Opinion Stage
-		$getUrlFormAction = 'https://www.opinionstage.com/integrations/wordpress/new';
+		$getUrlFormAction = OPINIONSTAGE_SERVER_BASE.'/integrations/wordpress/new';
 
 		// Opninionstge logo image link
 		$logoImagelinkOs = plugin_dir_url( __FILE__ ) . 'image/os-logo.png';
 
+		// Opinionstage view item count
+		$OsOptions = (array) get_option(OPINIONSTAGE_OPTIONS_KEY);
+
+
 		// Data to pass to gutenberg editor
 	    $dataToPass = array(
 	        'isOsConnected'         => (isset($os_options['uid']) && $os_options['uid'] != '') ? true : false,
-	        'onCreateButtonClickOs' => 'https://www.opinionstage.com/api/wp/redirects/widgets/new',
+	        'onCreateButtonClickOs' => OPINIONSTAGE_SERVER_BASE.'/api/wp/redirects/widgets/new',
 	        'callbackUrlOs'         => $adminUrlForOs,
 	        'OswpPluginVersion'     => $OswpPluginVersion,
 	        'OswpClientToken'       => opinionstage_user_access_token(),
 	        'OswpFetchDataUrl'      => $FetchUrlOS,
 	        'getActionUrlOS'        => $getUrlFormAction,
 	        'getLogoImageLink'		=> $logoImagelinkOs,
+	        'getOsOption'			=> $OsOptions,
 	    );
     	wp_localize_script( 'opinionStage_poll_oswp_block_js_set', 'osGutenData', $dataToPass );
 }
