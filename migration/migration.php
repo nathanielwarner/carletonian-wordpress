1498652dccdd360a19e9973a96e29f02dbe819a7
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-04-28 12:45:49 -0500

    Got migration almost ready

---
+++ migration/migration.php
@@ -0,0 +1,93 @@
+<?php
+
+require_once(__DIR__ . '/../wp-load.php');
+
+define("REASON_CATEGORY_NEWS", 345118);
+define("REASON_CATEGORY_SPORTS", 343990);
+define("REASON_CATEGORY_BALDSPOT", 1189200);
+define("REASON_CATEGORY_VIEWPOINT", 344001);
+define("REASON_CATEGORY_ARTSFEATURES", 722331);
+
+define("WP_CATEGORY_NONE", array(1));
+define("WP_CATEGORY_NEWS", array(3));
+define("WP_CATEGORY_SPORTS", array(4));
+define("WP_CATEGORY_BALDSPOT", array(5));
+define("WP_CATEGORY_VIEWPOINT", array(6));
+define("WP_CATEGORY_ARTSFEATURES", array(2));
+
+/*$post_to_insert = array(
+    'post_title'    => 'Testing 123',
+    'post_content'  => 'Content goes here',
+    'post_status'   => 'publish',
+    'post_author'   => 1,
+    'post_category' => array(3)
+);
+
+$result = wp_insert_post($post_to_insert, true);
+echo $result . "\n";*/
+
+function get_or_create_author($carl_user) {
+    $user = get_user_by('email', $carl_user . '@carleton.edu');
+    if ($user) {
+        $uid = $user->get('id');
+    } else {
+        //$uid = wp_create_user($carl_user, null, $carl_user . '@carleton.edu');
+        $uid = 999;
+    }
+    return $uid;
+}
+
+$reason_data = simplexml_load_file(__DIR__ . "/carletonian-reason-4-25.xml");
+//echo print_r($reason_data->entity[0]['type'], true);
+foreach ($reason_data->entity as $entity) {
+    foreach($entity->value as $value) {
+        switch($value['name']) {
+            case 'creation_date':
+                $date = (string) $value;
+                break;
+            case 'release_title':
+                $title = (string) $value;
+                break;
+            case 'created_by_username':
+                $author = get_or_create_author((string) $value);
+                break;
+            case 'content':
+                $content = (string) $value;
+        }
+        foreach($entity->relationships->alrel as $alrel) {
+            if ($alrel['name'] == 'news_to_news_section') {
+                switch ($alrel->rel['to_entity_id']) {
+                    case REASON_CATEGORY_NEWS:
+                        $category = WP_CATEGORY_NEWS;
+                        break;
+                    case REASON_CATEGORY_SPORTS:
+                        $category = WP_CATEGORY_SPORTS;
+                        break;
+                    case REASON_CATEGORY_BALDSPOT:
+                        $category = WP_CATEGORY_BALDSPOT;
+                        break;
+                    case REASON_CATEGORY_VIEWPOINT:
+                        $category = WP_CATEGORY_VIEWPOINT;
+                        break;
+                    case REASON_CATEGORY_ARTSFEATURES:
+                        $category = WP_CATEGORY_ARTSFEATURES;
+                        break;
+                    default:
+                        $category = WP_CATEGORY_VIEWPOINT;
+                        break;
+                }
+            }
+        }
+    }
+
+    $post_to_insert = array(
+        'post_title'    => $title,
+        'post_content'  => $content,
+        'post_status'   => 'publish',
+        'post_author'   => $author,
+        'post_category' => $category,
+        'post_date'     => $date,
+    );
+
+    echo print_r($post_to_insert, true);
+}
