3daccd2b28239c7f44a3cadd9d56f7b15d9ab9b3
Nathaniel Warner <nathanielwarner77@gmail.com>
2019-10-03 23:33:52 -0500

    Make author display nicer on author archives

--- wp-content/themes/mission-news-carletonian/content/archive-header.php
+++ wp-content/themes/mission-news-carletonian/content/archive-header.php
@@ -14,27 +14,29 @@ $prefix = esc_html_x( '', 'Posts published in CATEGORY', 'mission-news' );
 if ( is_tag() ) {
 	$icon_class = 'tag';
 	$prefix = esc_html__( 'Articles tagged as', 'mission-news' );
-} elseif ( is_author() ) {
-	$icon_class = 'user';
-	$prefix = esc_html_x( '', 'Posts published by AUTHOR', 'mission-news' );
 } elseif ( is_date() ) {
 	$icon_class = 'calendar';
 	// Repeating default value to add new translator note - context may change word choice
 	$prefix = esc_html_x( 'Articles published in', 'Posts published in MONTH', 'mission-news' );
 }
 ?>
-
-<div class='archive-header'>
+<?php if ( is_author() ) : ?><div class="loop-container"><div class="post-author"><?php else : ?><div class='archive-header'><?php endif ?>
 	<?php if ( get_theme_mod( 'archive_title' ) != 'no' ) : ?>
 	<h1>
-		<i class="far fa-<?php echo esc_attr( $icon_class ); ?>"></i>
-		<?php
-		echo esc_html( $prefix ) . ' ';
-		the_archive_title( '', '' );
-		?>
+        <?php if (is_author() ) : ?>
+            <div class="avatar-container">
+                <?php echo get_avatar( get_the_author_meta( 'ID' ), 78, '', get_the_author() ) ?>
+            </div>
+        <?php else : ?>
+            <i class="far fa-<?php echo esc_attr( $icon_class )?>"></i>
+        <?php endif;
+        echo esc_html( $prefix ) . ' ';
+        the_archive_title( '', '' );
+        ?>
 	</h1>
 	<?php endif;
 	if ( get_the_archive_description() != '' && get_theme_mod( 'archive_description' ) != 'no' ) :
 		the_archive_description();
 	endif; ?>
+<?php if ( is_author() ) : ?></div><?php endif ?>
 </div>
