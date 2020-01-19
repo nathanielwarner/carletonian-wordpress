d384dca23afb89d565b723aa0090188e02876e34
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-01-19 16:46:06 -0600

    Display all coauthors at the bottom of posts

--- wp-content/themes/mission-news-carletonian/content/post-author.php
+++ wp-content/themes/mission-news-carletonian/content/post-author.php
@@ -1,12 +1,23 @@
-<?php if ( get_theme_mod( 'author_box_posts' ) == 'no' ) return; ?>
-<div class="post-author">
-	<?php if ( get_theme_mod( 'author_avatar_posts' ) != 'no' ) : ?>
-	<div class="avatar-container">
-		<?php echo get_avatar( get_the_author_meta( 'ID' ), 78, '', get_the_author() ); ?>
-	</div>
-	<?php endif; ?>
-	<div>
-		<div class="author"><?php the_author(); ?></div>
-		<p><?php the_author_meta('description'); ?></p>
-	</div>
-</div>
+<?php
+
+if ( get_theme_mod( 'author_box_posts' ) == 'no' ) return;
+
+$coauthors = array(get_the_author());
+if ( function_exists('get_coauthors') ) {
+    $coauthors = get_coauthors();
+}
+
+foreach ($coauthors as $coauthor) { ?>
+    <div class="post-author"> <?php
+        if ( get_theme_mod( 'author_avatar_posts' ) != 'no' ) { ?>
+            <div class="avatar-container">
+                <?php echo get_avatar( get_the_author_meta( 'ID' , $coauthor->ID), 78, '', $coauthor->display_name );?>
+            </div> <?php
+        } ?>
+        <div>
+            <div class="author"><?php echo $coauthor->display_name ?></div>
+            <p><?php the_author_meta('description', $coauthor->ID); ?></p>
+        </div>
+    </div> <?php
+}
+?>
