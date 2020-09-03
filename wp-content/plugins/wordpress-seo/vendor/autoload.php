9c8020274fb380392a0a3b952b0944dc1805e4e1
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-09-03 03:43:51 +0000

    WP and plugin updates

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit69c430ed36a8ee92b13fbb33b8a9373d::getLoader();
+return ComposerAutoloaderInitcf563d3d240745ed37266229ad7db5d6::getLoader();
